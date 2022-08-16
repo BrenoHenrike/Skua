7d08f466faf84fb9340a778b4bfa0802b7ef5d5c
Breno Henrike <bhenrike@protonmail.com>
2022-08-16 16:13:21 -0300

    0.5

--- Skua.App.WPF/Properties/Settings.Designer.cs
+++ Skua.App.WPF/Properties/Settings.Designer.cs
@@ -202,7 +202,7 @@ namespace Skua.App.WPF.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("0.4.0.0")]
+        [global::System.Configuration.DefaultSettingValueAttribute("0.5.0.0")]
         public string ApplicationVersion {
             get {
                 return ((string)(this["ApplicationVersion"]));
