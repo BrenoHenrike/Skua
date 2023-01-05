7414c040b112daa367b7bc33c9bae759fa111261
Sly Kint A. Bacalso <slykint.bacalso@cit.edu>
2023-01-05 18:38:06 +0800

    Add code documentation of IScriptAuras interface

--- Skua.Core.Interfaces/Scripts/Auras/IScriptAuras.cs
+++ Skua.Core.Interfaces/Scripts/Auras/IScriptAuras.cs
@@ -3,7 +3,29 @@
 namespace Skua.Core.Interfaces;
 public interface IScriptAuras
 {
+    /// <summary>
+    /// Gets the auras of the specified subject type.
+    /// </summary>
+    /// <returns>
+    /// <see cref="IEnumerable{T}"/> of <see cref="Aura"/>s.
+    /// </returns>
     IEnumerable<Aura>? GetAuras();
+
+    /// <summary>
+    /// Checks if the subtype has active <paramref name="auraName"/>.
+    /// </summary>
+    /// <param name="auraName">The aura name.</param>
+    /// <returns>
+    /// <see cref="bool"/> true if subject has the aura; otherwise false
+    /// </returns>
     bool HasActiveAura(string auraName);
+
+    /// <summary>
+    /// Gets the aura of a subject type with the specified aura name.
+    /// </summary>
+    /// <param name="auraName">The aura name.</param>
+    /// <returns>
+    /// <see cref="Aura"/> object
+    /// </returns>
     Aura? GetAura(string auraName);
 }
