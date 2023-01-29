3d548e3356079185c84a809410c476d2bc2927c8
Sly Kint A. Bacalso <slykint.bacalso@cit.edu>
2023-01-29 11:23:13 +0800

    Add ClientFileSources static model

---
+++ Skua.Core.Models/ClientFileSources.cs
@@ -0,0 +1,12 @@
+﻿namespace Skua.Core.Models;
+
+public static class ClientFileSources
+{
+    public static string SkuaDIR { get; } = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "Skua");
+    public static string SkuaScriptsDIR { get; } = Path.Combine(SkuaDIR, "Scripts");
+    public static string SkuaThemesDIR { get; } = Path.Combine(SkuaDIR, "themes");
+    public static string SkuaOptionsDIR { get; } = Path.Combine(SkuaDIR, "options");
+    public static string SkuaPluginsDIR { get; } = Path.Combine(SkuaDIR, "plugins");
+    public static string SkuaAdvancedSkillsFile { get; } = Path.Combine(SkuaDIR, "AdvancedSkills.txt");
+    public static string SkuaQuestsFile { get; } = Path.Combine(SkuaDIR, "Quests.txt");
+}
