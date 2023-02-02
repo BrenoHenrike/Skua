e4290c17cd4a9b6c6c5f52c4e314e6219a6ec3d0
Sly Kint A. Bacalso <slykint.bacalso@cit.edu>
2023-02-02 15:25:06 +0800

    Remove Open VSCode in ScriptRepoView

--- Skua.Core/ViewModels/ScriptRepo/ScriptInfoViewModel.cs
+++ Skua.Core/ViewModels/ScriptRepo/ScriptInfoViewModel.cs
@@ -2,6 +2,7 @@
 using CommunityToolkit.Mvvm.Input;
 using CommunityToolkit.Mvvm.Messaging;
 using Skua.Core.Messaging;
+using Skua.Core.Models;
 using Skua.Core.Models.GitHub;
 
 namespace Skua.Core.ViewModels;
@@ -19,6 +20,7 @@ public partial class ScriptInfoViewModel : ObservableObject
     public int Size => Info.Size;
     public string LocalFile => Info.LocalFile;
     public string FilePath => Info.FilePath;
+    public string ScriptPath => Path.Combine(ClientFileSources.SkuaScriptsDIR, FilePath.Replace("/", "\\"));
     public string Tags => string.Join(", ", Info.Tags);
 
     [ObservableProperty]
