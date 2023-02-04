bfb517b4e345e1bc60a78ed266de03ee6dba6443
Sly Kint A. Bacalso <slykint.bacalso@cit.edu>
2023-02-04 14:50:53 +0800

    Update version to 1.2

--- Skua.App.WPF/Properties/Settings.Designer.cs
+++ Skua.App.WPF/Properties/Settings.Designer.cs
@@ -178,7 +178,7 @@ namespace Skua.App.WPF.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("1.1.5.0")]
+        [global::System.Configuration.DefaultSettingValueAttribute("1.2.0.0")]
         public string ApplicationVersion {
             get {
                 return ((string)(this["ApplicationVersion"]));
