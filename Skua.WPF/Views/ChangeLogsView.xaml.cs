ec03aa5a8bf6eafc1eff2342a9402331d16d342c
Sly Kint A. Bacalso <slykint.bacalso@cit.edu>
2023-02-06 00:11:09 +0800

    Add ChangeLogsView UserControl Page

---
+++ Skua.WPF/Views/ChangeLogsView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+using Skua.Core.ViewModels;
+
+namespace Skua.WPF.Views;
+/// <summary>
+/// Interaction logic for ChangeLogsView.xaml
+/// </summary>
+public partial class ChangeLogsView : UserControl
+{
+    public ChangeLogsView()
+    {
+        InitializeComponent();
+    }
+}
