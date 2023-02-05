e7dbc0bc54d5d4b8f2fe1554692548da5c6fd079
Sly Kint A. Bacalso <slykint.bacalso@cit.edu>
2023-02-06 01:21:38 +0800

    Implement the get change logs content

--- Skua.Core/ViewModels/ChangeLogsViewModel.cs
+++ Skua.Core/ViewModels/ChangeLogsViewModel.cs
@@ -1,8 +1,28 @@
-﻿namespace Skua.Core.ViewModels;
+﻿using Skua.Core.Utils;
+
+namespace Skua.Core.ViewModels;
 public class ChangeLogsViewModel : BotControlViewModelBase
 {
+    private string _markDownContent = "Loading content...";
+    
     public ChangeLogsViewModel() : base("Change Logs")
     {
-         
+        _markDownContent = string.Empty;
+        Task.Run(async () => await GetChangeLogsContent());
+    }
+
+    public string MarkdownDoc
+    {
+        get { return _markDownContent; }
+        set { SetProperty(ref _markDownContent, value); }
+    }
+
+    private async Task GetChangeLogsContent()
+    {
+        var response = await HttpClients.Default.GetAsync("https://raw.githubusercontent.com/BrenoHenrike/Skua/op-version/changelogs.md");
+        if (!response.IsSuccessStatusCode)
+            MarkdownDoc = "### No content found. Please check your internet connection.";
+
+        MarkdownDoc = await response.Content.ReadAsStringAsync();
     }
 }
