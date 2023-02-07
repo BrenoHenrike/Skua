82505ce93a5f4fd8c2ba35a0e6ae39c4869b0254
Sly Kint A. Bacalso <slykint.bacalso@cit.edu>
2023-02-07 18:42:32 +0800

    Make changelogs content disposable

--- Skua.Core/ViewModels/ChangeLogsViewModel.cs
+++ Skua.Core/ViewModels/ChangeLogsViewModel.cs
@@ -1,7 +1,6 @@
 ﻿using CommunityToolkit.Mvvm.DependencyInjection;
 using CommunityToolkit.Mvvm.Input;
 using Skua.Core.Interfaces;
-using Skua.Core.Utils;
 
 namespace Skua.Core.ViewModels;
 public class ChangeLogsViewModel : BotControlViewModelBase
@@ -11,6 +10,7 @@ public class ChangeLogsViewModel : BotControlViewModelBase
     public ChangeLogsViewModel() : base("Change Logs", 460, 500)
     {
         _markDownContent = string.Empty;
+        
         Task.Run(async () => await GetChangeLogsContent());
 
         OpenDelfinaDonationLink = new RelayCommand(() => Ioc.Default.GetRequiredService<IProcessService>().OpenLink("https://www.paypal.com/donate/?hosted_button_id=DMZFDRYJ5BT96"));
@@ -29,10 +29,13 @@ public class ChangeLogsViewModel : BotControlViewModelBase
 
     private async Task GetChangeLogsContent()
     {
-        var response = await HttpClients.Default.GetAsync("https://raw.githubusercontent.com/BrenoHenrike/Skua/op-version/changelogs.md");
-        if (!response.IsSuccessStatusCode)
-            MarkdownDoc = "### No content found. Please check your internet connection.";
+        using (var client = new HttpClient())
+        {
+            var response = await client.GetAsync("https://raw.githubusercontent.com/BrenoHenrike/Skua/op-version/changelogs.md");
+            if (!response.IsSuccessStatusCode)
+                MarkdownDoc = "### No content found. Please check your internet connection.";
 
-        MarkdownDoc = await response.Content.ReadAsStringAsync();
+            MarkdownDoc = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+        }
     }
 }
