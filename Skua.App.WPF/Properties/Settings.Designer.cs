84a12786cadb5313321a450f6397b391d239f4d0
Breno Henrike <bhenrike@protonmail.com>
2023-03-17 13:26:34 -0300

    Version 1.2.2.1

--- Skua.App.WPF/Properties/Settings.Designer.cs
+++ Skua.App.WPF/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace Skua.App.WPF.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.3.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.5.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -178,7 +178,7 @@ namespace Skua.App.WPF.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("1.2.2.0")]
+        [global::System.Configuration.DefaultSettingValueAttribute("1.2.2.1")]
         public string ApplicationVersion {
             get {
                 return ((string)(this["ApplicationVersion"]));
