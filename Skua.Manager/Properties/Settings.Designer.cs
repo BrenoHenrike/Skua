edc3605d33d4da33c08b8dbe0e03f3c2f165b067
Purple <koen.t04@gmail.com>
2024-04-04 06:17:44 -0500

    Version bump+Post builds for Installer
    Version bump might be temporary and the build events for the installer was wack so I fixed it

--- Skua.Manager/Properties/Settings.Designer.cs
+++ Skua.Manager/Properties/Settings.Designer.cs
@@ -110,7 +110,7 @@ namespace Skua.Manager.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("1.2.3.1")]
+        [global::System.Configuration.DefaultSettingValueAttribute("1.2.4.0")]
         public string ApplicationVersion {
             get {
                 return ((string)(this["ApplicationVersion"]));
